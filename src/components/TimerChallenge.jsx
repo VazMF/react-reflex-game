3d9c3cfbac03ce6d0cda420fbf332d14df926557
VazMF <vazfernandam@gmail.com>
2026-02-25 01:33:05 -0300

    feat: implements timer logic using react ref

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,40 @@
+import { useRef, useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
+  const [timeStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    setTimerStarted(true);
+
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+    setTimerStarted(false);
+    setTimerExpired(false);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button ref={timer} onClick={timeStarted ? handleStop : handleStart}>
+          {timeStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="active">Time is running</p>
+      <p className={timeStarted ? "active" : undefined}>
+        {timeStarted ? "Timer is running" : "Timer inactive"}
+      </p>
     </section>
   );
 }
